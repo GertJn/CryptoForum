5bdb583028af6fb7552f420eba1ec9e6b8007280
GertJn <gertjanson@hotmail.com>
2021-07-21 21:39:10 +0300

    Update views.py
    Adding post creation, comment creation and home page views.

--- Forum/CryptoForum/views.py
+++ Forum/CryptoForum/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
-# Create your views here.
+
+def home(request):
+    forums = Forum.objects.all()
+    count = forums.count()
+    comments = []
+    for i in forums:
+        comments.append(i.comments_set.all())
+
+    context = {'forums': forums,
+               'count': count,
+               'discussions': discussions}
+    return render(request, 'home.html', context)
+
+
+def addInForum(request):
+    form = CreateInForum()
+    if request.method == 'POST':
+        form = CreateInForum(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'addInForum.html', context)
+
+
+def addInComments(request):
+    form = CreateInComments()
+    if request.method == 'POST':
+        form = CreateInComments(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'addInDiscussion.html', context)
+
