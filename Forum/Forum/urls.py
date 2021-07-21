0bcec435e87160d4ac65abca816fe3e426649ab6
GertJn <gertjanson@hotmail.com>
2021-07-21 22:41:28 +0300

    Fixin urls.py file
    Fixing errors with urls.py file.

--- Forum/Forum/urls.py
+++ Forum/Forum/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from CryptoForum.Forum.CryptoForum.views import *
+from CryptoForum.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
