9b82e074ddad5c50edbce51b44301d78506e2fea
GertJn <gertjanson@hotmail.com>
2021-07-21 20:56:46 +0300

    Adding 2 models into models.py
    Adding Forum and Comments models

--- Forum/CryptoForum/models.py
+++ Forum/CryptoForum/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# parent model
+class Forum(models.Model):
+    name = models.CharField(max_length=200, default="anonymous")
+    topic = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000, blank=True)
+    link = models.CharField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.topic)
+
+
+# child model because of a foreignkey from Forum class
+class Comments(models.Model):
+    forum = models.ForeignKey(Forum, blank=True, on_delete=models.CASCADE)
+    discuss = models.CharField(max_length=1000)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.forum)
